e66bb042d71fb2bd9651c0c4a5d39410e1284e73
JungHwan <kinwjdghks@naver.com>
2024-04-27 02:17:56 +0900

    gps code added but no marker showing

--- app/src/main/java/edu/skku/map/capstone/HomeFragment.kt
+++ app/src/main/java/edu/skku/map/capstone/HomeFragment.kt
@@ -1,27 +1,47 @@
 package edu.skku.map.capstone
 
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
-import edu.skku.map.capstone.databinding.FragmentFavoriteBinding
+import com.kakao.vectormap.label.Label
+import com.kakao.vectormap.label.LabelLayer
+import com.kakao.vectormap.label.LabelManager
+import com.kakao.vectormap.label.LabelOptions
+import com.kakao.vectormap.label.LabelStyle
+import com.kakao.vectormap.label.LabelStyles
+import com.kakao.vectormap.label.TrackingManager
 import edu.skku.map.capstone.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment() : Fragment() {
     private var _binding:FragmentHomeBinding? = null
     private val binding get() = _binding!!
-
-
+    private lateinit var locationManager:LocationManager
+    private lateinit var locationListener: LocationListener
+    private lateinit var labelManager:LabelManager
+    private lateinit var trackingManager:TrackingManager
+    private var myPosition:Label? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        setLocationManager()
+        setListener()
+        setLocationListener()
 
 
         binding.kakaoMV.start(object : MapLifeCycleCallback() {
@@ -35,14 +55,91 @@ class HomeFragment : Fragment() {
         }, object : KakaoMapReadyCallback() {
             override fun onMapReady(kakaoMap: KakaoMap) {
                 Log.d("@@@ kakao map ready", "!")
+                labelManager = kakaoMap.labelManager!!
+                trackingManager = kakaoMap.trackingManager!!
             }
         })
 
-
-
-
         return binding.root
     }
+
+
+    private fun setLocationManager(){
+        Log.d("gps", "initializeLocationObj")
+        if(::locationManager.isInitialized.not()) {
+            locationManager = requireActivity().getSystemService(LOCATION_SERVICE) as LocationManager
+        }
+    }
+    private fun setLocationListener() {
+        if(::locationListener.isInitialized.not()) {
+            locationListener = object : LocationListener {
+                override fun onLocationChanged(location: Location) {
+                    Log.d("gps", "onLocationChanged")
+                    var lat = 0.0
+                    var lng = 0.0
+                    if (location != null) {
+                        lat = location.latitude
+                        lng = location.longitude
+                        Log.d("gps", "lat: $lat, lng: $lng")
+                    }
+                    if(myPosition == null){
+                        myPosition = createLabel(lat, lng)
+                        trackingManager.startTracking(createLabel(lat,lng))
+                    }
+                    else{
+                        myPosition!!.moveTo(LatLng.from(lat,lng))
+                    }
+                    Log.d("gps", "label: $myPosition")
+                }
+
+                override fun onProviderEnabled(provider: String) {
+                    Log.d("gps", "onProviderEnabled")
+                }
+
+                override fun onProviderDisabled(provider: String) {}
+            }
+        }
+
+
+    }
+    private fun getCurrentLocation() {
+        Log.d("gps", "getCurrentLocation")
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("gps","requestLocationUpdates")
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                1500L,
+                30f,
+                locationListener
+            )
+
+        }
+        else{
+            Log.d("gps","permission needed")
+        }
+
+    }
+    private fun setListener() {
+        binding.gpsBtn.setOnClickListener {
+            getCurrentLocation()
+        }
+    }
+
+    private fun createLabel(lat: Double, lon: Double): Label {
+        val styles: LabelStyles =
+            labelManager.addLabelStyles(LabelStyles.from(LabelStyle.from(R.drawable.mymarker)))!!
+        val options: LabelOptions = LabelOptions.from(LatLng.from(lat, lon)).setStyles(styles)
+        val layer: LabelLayer = labelManager.layer!!
+        return layer.addLabel(options)
+    }
+
 
 
 }
--- app/src/main/java/edu/skku/map/capstone/MainActivity.kt
+++ app/src/main/java/edu/skku/map/capstone/MainActivity.kt
@@ -1,13 +1,22 @@
 package edu.skku.map.capstone
 
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.kakao.vectormap.KakaoMapSdk
 import edu.skku.map.capstone.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Array<String>>
+
 
     val viewModel = MainViewModel()
     private lateinit var FavoriteFragment: FavoriteFragment
@@ -16,18 +25,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var MyPageFragment: MyPageFragment
 
 
+    private val permissions = arrayOf(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         KakaoMapSdk.init(this, "09e7ce580fee2dc13ec5d24c66cd8238")
-
         viewModel.fetchData()
+        setActivityResultLauncher()
+        resolvePermission(permissions)
 
-
-
-
-        navActions()
+        setNavActions()
         setUI()
     }
     private fun setUI(){
@@ -35,7 +49,8 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().add(binding.frameLayout.id, HomeFragment).commit()
 
     }
-    private fun navActions() {
+    private fun setNavActions() {
+
         HomeFragment = HomeFragment()
         FavoriteFragment = FavoriteFragment()
         MyPageFragment = MyPageFragment()
@@ -52,6 +67,24 @@ class MainActivity : AppCompatActivity() {
         }
         binding.myPageBtn.setOnClickListener {
             supportFragmentManager.beginTransaction().replace(binding.frameLayout.id, MyPageFragment).commit()
+        }
+    }
+    private fun setActivityResultLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            if (it.all { permission -> permission.value }) {
+
+            } else {
+                Toast.makeText(this, "권한 거부", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun resolvePermission(permissions: Array<String>) {
+        val res = permissions.all {
+            ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+        }
+        Log.d("permission",res.toString())
+        if(!res){
+            activityResultLauncher.launch(permissions)
         }
     }
 }
