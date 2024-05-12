d10d1448ea8b031e9386b1b8e81d91225fb34603
Ji__ho <cale.jiho@gmail.com>
2024-05-12 21:00:24 +0900

    ✨ Feat: policies
    polices to notion

--- app/src/main/java/edu/skku/map/capstone/fragments/MyPageFragment.kt
+++ app/src/main/java/edu/skku/map/capstone/fragments/MyPageFragment.kt
@@ -1,5 +1,7 @@
 package edu.skku.map.capstone.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -17,8 +19,23 @@ class MyPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMyPageBinding.inflate(inflater, container, false)
-        binding.cardView.setOnClickListener{Log.d("@@@","아무거나")}
+        setPolicyClickListeners()
         return binding.root
     }
 
+    private fun setPolicyClickListeners() {
+        binding.personalInfoAgreement.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://metal-ocelot-ebc.notion.site/e8f6f619e5944f7289c503be2e87b13e?pvs=4"))
+            startActivity(intent)
+        }
+        binding.serviceAgreement.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://metal-ocelot-ebc.notion.site/e9514d03f5d74eba9a6f0ee3e4c13ab5?pvs=4"))
+            startActivity(intent)
+        }
+        binding.placeAgreement.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://metal-ocelot-ebc.notion.site/7eca22780d9b4d869afc0be5bdca221e?pvs=4"))
+            startActivity(intent)
+        }
+
+    }
 }
